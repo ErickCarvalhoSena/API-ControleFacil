bc8971c01b0e25ccc675838acca0762feb1bd086
Erick Sena <erick087sena@gmail.com>
2026-05-18 22:10:07 -0300

    Implementando métodos no service de usuário

--- src/ControleFacil.Api/Damain/Services/Classes/UsuarioService.cs
+++ src/ControleFacil.Api/Damain/Services/Classes/UsuarioService.cs
@@ -34,6 +34,50 @@ namespace ControleFacil.Api.Damain.Services.Classes
             return _mapper.Map<UsuarioResponseContract>(usuario);
         }
 
+
+        public async Task<UsuarioResponseContract> Atualizar(long id, UsuarioRequestContract entidade, long idUsuario)
+        {
+           _ = await Obter(id) ?? throw new Exception("Usuario não encontrado para atualizar");
+
+           var usuario = _mapper.Map<Usuario>(entidade);
+           usuario.Id = id;
+           usuario.Senha = GerarHashSenha(entidade.Senha);
+
+           usuario = await _usuarioRepository.Atualizar(usuario);
+
+           return _mapper.Map<UsuarioResponseContract>(usuario);
+
+        }
+
+        public Task<UsuarioLoginResponseContract> Autenticar(UsuarioLoginRequestContract usuarioLoginRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Inativar(long id, long idUsuario)
+        {
+            var usuario = await Obter(id) ?? throw new Exception("Usuario não encontrado para invativar");
+
+            await _usuarioRepository.Deletar(_mapper.Map<Usuario>(usuario));
+        }
+
+        public async Task<IEnumerable<UsuarioResponseContract>> Obter(long idUsuario)
+        {
+            return await Obter(idUsuario);
+        }
+
+        public async Task<UsuarioResponseContract> Obter(long id, long idUsuario)
+        {
+           var usuario = await _usuarioRepository.Obter(id);
+            return _mapper.Map<UsuarioResponseContract>(usuario);
+        }
+
+        public async Task<UsuarioResponseContract> Obter(string email)
+        {
+            var usuario = await _usuarioRepository.Obter(email);
+            return _mapper.Map<UsuarioResponseContract>(usuario);
+        }
+
         private string GerarHashSenha(string senha)
         {
             string hashSenha;
@@ -46,31 +90,6 @@ namespace ControleFacil.Api.Damain.Services.Classes
 
                 return hashSenha;
             }
-        }
-
-        public Task<UsuarioResponseContract> Atualizar(long id, UsuarioRequestContract entidade, long idUsuario)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<UsuarioLoginResponseContract> Autenticar(UsuarioLoginRequestContract usuarioLoginRequest)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Inativar(long id, long idUsuario)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<UsuarioResponseContract>> Obter(long idUsuario)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<UsuarioResponseContract> Obter(long id, long idUsuario)
-        {
-            throw new NotImplementedException();
         }
     }
 }
